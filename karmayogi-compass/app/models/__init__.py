1552c9115426db482c6d62fc91bd8eb056393c2d
Shaeel-Hasepatil <shaeelhasepatil@gmail.com>
2026-09-25 09:03:25 +0530

    Remove duplicate folder and tracked pyc files; tighten .gitignore

--- karmayogi-compass/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.user import User
-from app.models.competency import CompetencyProfile
-from app.models.course import Course
-from app.models.assessment import Assessment
-from app.models.passport import CompetencyPassport
-
-__all__ = ["User", "CompetencyProfile", "Course", "Assessment", "CompetencyPassport"]
